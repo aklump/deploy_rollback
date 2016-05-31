8ff4b33af2472bc13ef46ecb48d72725ca5af8be
aklump <sourcecode@intheloftstudios.com>
2016-05-31 16:38:20 -0700

    fixed tmpdir problem

--- includes/preroute.sh
+++ includes/preroute.sh
@@ -10,7 +10,7 @@ if [ "$rb_op" == "init" ] || [ "$rb_op" == "help" ]; then
 fi
 
 # Verify dependencies
-if [ ! $rb_git ] ; then
+if [ ! "$rb_git" ] ; then
    lobster_failed "Git must be installed on system; see \$rb_git  in $LOBSTER_APP_ROOT/$lobster_app_config"
 fi
 
@@ -20,11 +20,11 @@ if [ ! "$rb_drupal_root" ]; then
 fi
 
 # Verify drupal root exists.
-if [ ! -d "$rb_drupal_root" ] || ! (cd $rb_drupal_root && is_drupal_root); then
+if [ ! -d "$rb_drupal_root" ] || ! (cd "$rb_drupal_root" && is_drupal_root); then
   lobster_failed "$rb_drupal_root does not exist or does not appear to be a Drupal project."
 fi
 
 # Make sure we have the rollback directory
-[ ! -d $rb_data_dir ] || mkdir -p $rb_data_dir
+[ ! -d "$rb_data_dir" ] || mkdir -p "$rb_data_dir"
 
 
