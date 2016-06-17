ce09f468bf6db392162e4ee9b2ee29edd3d9c0eb
aklump <sourcecode@intheloftstudios.com>
2016-06-16 18:37:51 -0700

    Version bumped from 0.1.31 to 0.1.32

--- .web_package/hooks/build/version_update.php
+++ .web_package/hooks/build/version_update.php
@@ -12,7 +12,7 @@ if ($contents = $before = file_get_contents($filename)) {
   $contents = preg_replace('/^rb_version=["\d\.]+/m', 'rb_version="' . $argv[2] . '"', $contents);
   $changed = $contents !== $before;
   if ($changed && file_put_contents($filename, $contents)) {
-    echo "Version string updated to " . $argv[2] . " in $(basename filename)";
+    echo "Version string updated to " . $argv[2] . " in " . basename($filename);
     return;
   }
 }
