cf4319f7c52d8ddabe050e2e096ab1f956f9b93b
aklump <sourcecode@intheloftstudios.com>
2016-07-05 21:11:43 -0700

    fixed path to lib

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -1,3 +1,2 @@
 <?php
-require_once dirname(__FILE__) . '/../lib/loft_php_lib/src/AKlump/LoftLib/Drupal/DrupalBridge.php';
-//require_once dirname(__FILE__) . '/../lib/loft_php_lib/dist/vendor/autoload.php';
+require_once dirname(__FILE__) . '/../lib/loft_php_lib/dist/src/AKlump/LoftLib/Drupal/DrupalBridge.php';
